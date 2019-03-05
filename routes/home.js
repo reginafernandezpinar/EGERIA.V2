3adb816467bcb71d1dc07c403f061088def0c540
reginafernandezpinar <regina.fernandez.p@gmail.com>
2019-03-05 19:07:10 +0100

    login prof version

--- routes/home.js
+++ routes/home.js
@@ -3,23 +3,28 @@ var router = express.Router();
 const tripControllers = require ('../controllers/tripController');
 const Token = require('../auth/token');
 
-/* GET home page. */
+/* GET public home page. */
 router.get('/', function(req, res, next) {
   res.render('home');
 });
+// Get featured/all trips
+router.get('/mytrips/api/trips', tripControllers.findAll);
+// Get a trip
+router.get('/mytrips/api/trips/:id', tripControllers.findOne);
 
 
+
+/* GET private home page. */
 router.get('/token/:token', Token.verifyParam, function(req, res, next) { 
   res.render('homelogin', {token: req.params.token});
 });
 
 
 // // estas rutas están en trip.js, pendiente ruta de la peticion del front main.js 
-// // Get featured/all trips
-// router.get('/:token/api/trips', tripControllers.findAll);
 
-// // // Get a trip
-// router.get('/:token/api/trips/:id', tripControllers.findOne);
+
+
+
 
 
 module.exports = router;
